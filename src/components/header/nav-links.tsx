456cde291d0340239f73b04970331b01a740b53a
Stefan Vitoria <steph.74lw@gmail.com>
2024-06-28 19:25:54 -0100

    style: NavLinks text color and hover efects.

--- src/components/header/nav-links.tsx
+++ src/components/header/nav-links.tsx
@@ -15,7 +15,7 @@ const NavLinks: React.FC<NavLinksProps> = (props: NavLinksProps) => {
 
         <nav
             className="fixed flex top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
-            <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+            <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium sm:w-[initial] sm:flex-nowrap sm:gap-5">
                 {
                     links.map(link => (
                         <motion.li
@@ -26,7 +26,7 @@ const NavLinks: React.FC<NavLinksProps> = (props: NavLinksProps) => {
                         >
                             <Link
                                 className={cn(
-                                    "flex w-full items-center justify-center p-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
+                                    "flex w-full items-center justify-center p-3 hover:text-gray-950 transition dark:hover:text-gray-300",
                                     { "text-gray-950 dark:text-gray-200": activeSection === link.name }
                                 )}
                                 href={link.href}
