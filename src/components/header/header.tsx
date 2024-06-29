8675d8a180d62610513c06da2f5d10c9afc34d91
Stefan Vitoria <steph.74lw@gmail.com>
2024-06-29 14:26:01 -0100

    fix: Header bg opacity.

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -11,7 +11,7 @@ const Header = (props: HeaderProps) => {
 
         <header className='z-[1000] relative'>
             <motion.div
-                className='fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none shadow-sm backdrop-blur-[0.5rem]
+                className='fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none shadow-sm bg-background/70 backdrop-blur-[0.5rem]
              sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full'
 
                 initial={{ y: -100, x: "-50%", opacity: 0 }}
